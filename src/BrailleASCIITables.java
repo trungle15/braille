122e0b065b87db2488e794d53523baeaccb983be
trungle15 <50526463+trungle15@users.noreply.github.com>
2024-04-23 20:21:06 -0500

    Change indentation to space

--- src/BrailleASCIITables.java
+++ src/BrailleASCIITables.java
@@ -8,34 +8,34 @@ import java.security.InvalidKeyException;
  * @author Trung Le
  */
 public class BrailleASCIITables {
-	
-	private static BitTree ASCIIToBraille = new BitTree(8);
-	private static BitTree BrailleToASCII = new BitTree(6);
-	private static BitTree BrailleToUnicode = new BitTree(6);
+  
+  private static BitTree ASCIIToBraille = new BitTree(8);
+  private static BitTree BrailleToASCII = new BitTree(6);
+  private static BitTree BrailleToUnicode = new BitTree(6);
 
-	static {
-		try {
-			ASCIIToBraille.load(new FileInputStream("src/mappings/ASCIItoBraille.csv"));
-			BrailleToASCII.load(new FileInputStream("src/mappings/BrailletoASCII.csv"));
-			BrailleToUnicode.load(new FileInputStream("src/mappings/BrailletoUnicode.csv"));
-		} // try
-		catch (InvalidKeyException | IOException e) {
-			e.printStackTrace();
-		} // catch
-	} // static
+  static {
+    try {
+      ASCIIToBraille.load(new FileInputStream("src/mappings/ASCIItoBraille.csv"));
+      BrailleToASCII.load(new FileInputStream("src/mappings/BrailletoASCII.csv"));
+      BrailleToUnicode.load(new FileInputStream("src/mappings/BrailletoUnicode.csv"));
+    } // try
+    catch (InvalidKeyException | IOException e) {
+      e.printStackTrace();
+    } // catch
+  } // static
 
 
   static String toBraille(char letter) throws InvalidKeyException {
-		int l = (int) letter;
-		String binary = "0" + Integer.toBinaryString(l);
-		return ASCIIToBraille.get(binary);
-	} // String toBraille
+    int l = (int) letter;
+    String binary = "0" + Integer.toBinaryString(l);
+    return ASCIIToBraille.get(binary);
+  } // String toBraille
 
-	static String toASCII(String bits) throws InvalidKeyException {
-		return BrailleToASCII.get(bits);
-	} // String toASCII
+  static String toASCII(String bits) throws InvalidKeyException {
+    return BrailleToASCII.get(bits);
+  } // String toASCII
 
-	static String toUnicode(String c) throws InvalidKeyException {
-		return BrailleToUnicode.get(c);
-	} // String toUnicode
+  static String toUnicode(String c) throws InvalidKeyException {
+    return BrailleToUnicode.get(c);
+  } // String toUnicode
 } // class BrailleASCIITables
