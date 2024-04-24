f334333d1d0b08efa80859226f394aea721a507a
trungle15 <50526463+trungle15@users.noreply.github.com>
2024-04-23 19:28:56 -0500

    remove main

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Integer a = (int) 'Z';
-        String binary = "0" + Integer.toBinaryString(a);
-        System.out.println(binary);
-    }
-}
