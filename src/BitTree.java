c976eee3d6abbbee4632b7c5667a177dbeb79b89
trungle15 <50526463+trungle15@users.noreply.github.com>
2024-04-23 20:09:36 -0500

    Add javadoc

--- src/BitTree.java
+++ src/BitTree.java
@@ -5,6 +5,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.security.InvalidKeyException;
 
+/**
+ * Represents a binary tree that stores values associated with bit sequences.
+ * @author Trung Le
+ */
 public class BitTree {
 
   BitTreeNode root;
@@ -13,12 +17,12 @@ public class BitTree {
   public BitTree(int n) {
     this.root = new BitTreeNode();
     this.depth = n;
-  }
+  } // BitTree
 
   public void set(String bits, String value) throws InvalidKeyException {
     if (bits.length() != depth) {
       throw new InvalidKeyException("Invalid length for bits");
-    }
+    } // if (bits.length() != depth)
 
     BitTreeNode current = root;
 
@@ -29,17 +33,18 @@ public class BitTree {
 
       if (c != '0' && c != '1') {
         throw new InvalidKeyException("Invalid bit sequence. Bit can only be either 0 or 1");
-      }
+      } // if (c != '0' && c != '1')
 
 
       // Last character
       if (i == bits.length() - 1) {
         if (c == '0') {
           current.left = new BitTreeLeaf(value);
-        } else {
+        } // if (c == '0')
+        else {
           current.right = new BitTreeLeaf(value);
-        }
-      }
+        } // else
+      } // if (i == bits.length() - 1)
 
 
       else {
@@ -48,26 +53,26 @@ public class BitTree {
         if (c == '0') {
           if (current.left == null) {
             current.left = new BitTreeNode();
-          }
+          } // if (current.left == null)
           current = current.left;
-        }
+        } // if (c == '0')
 
         // Traverse right
         else {
           if (current.right == null) {
             current.right = new BitTreeNode();
-          }
+          } // if (current.right == null)
           current = current.right;
-        }
-      }
-    }
-  }
+        } // else
+      } // else
+    } // for (int i = 0; i < bits.length(); i++)
+  } // void set
 
   public String get(String bits) throws InvalidKeyException {
 
     if (bits.length() != depth) {
       throw new InvalidKeyException("Invalid length for bits");
-    }
+    } // if
 
     BitTreeNode current = root;
 
@@ -76,7 +81,7 @@ public class BitTree {
 
       if (c != '0' && c != '1') {
         throw new InvalidKeyException("Invalid bit sequence. Bit can only be either 0 or 1");
-      }
+      } // if
 
       if (c == '0') {
         if (current.left == null) {
@@ -84,43 +89,44 @@ public class BitTree {
         } else {
           current = current.left;
         }
-      } else {
+      } // if 
+      else {
         if (current.right == null) {
           throw new InvalidKeyException("No right path at bit index " + i);
-        } else {
+        } // if 
+        else {
           current = current.right;
-        }
-      }
-    }
+        } // else
+      } // else
+    } // for
 
     BitTreeLeaf ret = (BitTreeLeaf) current;
 
     return ret.getValue();
-  }
+  } // String get
 
   private void dumpHelper(PrintWriter pen, BitTreeNode node, String path) {
 
     if (node == null) {
       return;
-    }
+    } // if (node == null)
 
     if (node instanceof BitTreeLeaf) {
       BitTreeLeaf leaf = (BitTreeLeaf) node;
       pen.println(path + ',' + leaf.getValue());
-    }
-    else {
+    } else {
       if (node.left != null) {
         dumpHelper(pen, node.left, path + "0");
-      }
+      } // if (node.left != null)
       if (node.right != null) {
         dumpHelper(pen, node.right, path + "1");
-      }
-    }
-  }
+      } // if (node.right != null)
+    } // else
+  } // void dumpHelper
 
   public void dump(PrintWriter pen) {
     dumpHelper(pen, this.root, "");
-  }
+  } // void dump
 
   public void load(InputStream source) throws InvalidKeyException, IOException {
 
@@ -131,9 +137,9 @@ public class BitTree {
     while ((line = reader.readLine()) != null) {
       String[] fields = line.split(",");
       set(fields[0], fields[1]);
-    }
+    } // while
 
     source.close();
-    
-  }
-}
+
+  } // void load
+} // class BitTree
