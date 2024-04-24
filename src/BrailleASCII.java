baa51591f3e0d2ddc071c46f5e2545aa474ef33e
trungle15 <50526463+trungle15@users.noreply.github.com>
2024-04-23 20:20:12 -0500

    Space indentation

--- src/BrailleASCII.java
+++ src/BrailleASCII.java
@@ -8,61 +8,61 @@ import java.security.InvalidKeyException;
  */
 public class BrailleASCII {
 
-	static PrintWriter pen = new PrintWriter(System.out, true);
+  static PrintWriter pen = new PrintWriter(System.out, true);
 
-	public static void main(String[] args) throws InvalidKeyException {
+  public static void main(String[] args) throws InvalidKeyException {
 
-		if (args.length != 2) {
-			System.err.println("Invalid parameter amounts");
-			System.exit(1);
-		} // if
+    if (args.length != 2) {
+      System.err.println("Invalid parameter amounts");
+      System.exit(1);
+    } // if
 
-		else {
-			String command = args[0];
-			String input = args[1];
+    else {
+      String command = args[0];
+      String input = args[1];
 
-			if (command.equals("braille")) {
-				char[] chars = input.toCharArray();
-				for (char c : chars) {
-					pen.print(BrailleASCIITables.toBraille(c));
-				} // if
-				pen.println();
-			} // else
+      if (command.equals("braille")) {
+        char[] chars = input.toCharArray();
+        for (char c : chars) {
+          pen.print(BrailleASCIITables.toBraille(c));
+        } // if
+        pen.println();
+      } // else
 
-			else if (command.equals("ascii")) {
-				
-				int chunkSize = 6;
-				if (input.length() % chunkSize != 0) {
-					System.err.print("Invalid bit sequence length");
-					System.exit(1);
-				} // if
+      else if (command.equals("ascii")) {
+        
+        int chunkSize = 6;
+        if (input.length() % chunkSize != 0) {
+          System.err.print("Invalid bit sequence length");
+          System.exit(1);
+        } // if
 
-				for (int i = 0; i < input.length(); i += chunkSize) {
-					int end = i + chunkSize;
-					String substring = input.substring(i, end);
-					pen.print(BrailleASCIITables.toASCII(substring));
-				} // for
+        for (int i = 0; i < input.length(); i += chunkSize) {
+          int end = i + chunkSize;
+          String substring = input.substring(i, end);
+          pen.print(BrailleASCIITables.toASCII(substring));
+        } // for
 
-				pen.println();
+        pen.println();
 
-			} // else if
+      } // else if
 
-			else if (command.equals("unicode")) {
-				char[] chars = input.toCharArray();
-				for (char c : chars) {
-					String hexCode = BrailleASCIITables.toUnicode(BrailleASCIITables.toBraille(c));
-					int codePoint = Integer.parseInt(hexCode, 16);
-					char unicode = (char) codePoint;
-					pen.print(unicode);
-				} // for
-				pen.println();
-			} // else if
+      else if (command.equals("unicode")) {
+        char[] chars = input.toCharArray();
+        for (char c : chars) {
+          String hexCode = BrailleASCIITables.toUnicode(BrailleASCIITables.toBraille(c));
+          int codePoint = Integer.parseInt(hexCode, 16);
+          char unicode = (char) codePoint;
+          pen.print(unicode);
+        } // for
+        pen.println();
+      } // else if
 
-			else {
-				System.err.println("Invalid command");
-				System.exit(1);
-			} // else
-		} // else
-		pen.close();
-	} // main
+      else {
+        System.err.println("Invalid command");
+        System.exit(1);
+      } // else
+    } // else
+    pen.close();
+  } // main
 } // class BrailleASCII
